3d009a939d2a4fc14a9e1180aafbf51fa85eebd5
Tomas David <tdavid@redhat.com>
2019-12-06 19:31:55 +0100

    Add ESLint basic configuration

--- cypress/integration/LoginTest.ts
+++ cypress/integration/LoginTest.ts
@@ -1,12 +1,14 @@
 describe("Login test", () => {
     it("should login into workbench", () => {
-        const user: string = "testadmin"
-        const password:string="admin1234;"
+        const user = "testadmin"
+        const password = "admin1234;"
         cy.visit("http://localhost:8080/business-central/")
         cy.get("[name='j_username']").type(user)
         cy.get("[name='j_password']").type(password)
         cy.get(".btn-lg").click()
         cy.get("[data-field='welcome']").should("have.text", "Welcome to Red Hat Process Automation Manager")
         cy.get("#mega-menu").get("a:has(span.pficon-user)").should("have.text", user)
+
+
     })
 })
