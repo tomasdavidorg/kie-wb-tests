a83c315d9d8f5b04aadc8f73b1d750506de4be61
Tomas David <tdavid@redhat.com>
2019-12-06 17:49:44 +0100

    Add first test and project structure
    Signed-off-by: Tomas David <tdavid@redhat.com>

---
+++ cypress/plugins/index.js
@@ -0,0 +1,22 @@
+const wp = require('@cypress/webpack-preprocessor')
+module.exports = (on) => {
+  const options = {
+    webpackOptions: {
+      resolve: {
+        extensions: [".ts"]
+      },
+      module: {
+        rules: [
+          {
+            // all .ts files
+            test: /\.ts$/,
+            // ts loader for webpack 
+            loader: "ts-loader",
+            options: { transpileOnly: true }
+          }
+        ]
+      }
+    },
+  }
+  on('file:preprocessor', wp(options))
+}
