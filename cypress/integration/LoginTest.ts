ed9930b0bcd1c4887ee6ffcf75cff4350a8d89c2
Tomas David <tdavid@redhat.com>
2019-12-13 20:05:05 +0100

    Add login command

--- cypress/integration/LoginTest.ts
+++ cypress/integration/LoginTest.ts
@@ -3,9 +3,7 @@ describe("Login test", () => {
         const user = "testadmin"
         const password = "admin1234;"
         cy.visit("http://localhost:8080/business-central/")
-        cy.get("[name='j_username']").type(user)
-        cy.get("[name='j_password']").type(password)
-        cy.get(".btn-lg").click()
+        cy.login(user, password)
         cy.get("[data-field='welcome']").should("have.text", "Welcome to Red Hat Process Automation Manager")
         cy.get("#mega-menu").get("a:has(span.pficon-user)").should("have.text", user)
     })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    interface Chainable<Subject = any> {
+
+        login(user: string, password: string): Chainable<Element>;
+    }
+}
+
+Cypress.Commands.add("login", (user: string, password: string) => {
+    cy.get("[name='j_username']").type(user)
+    cy.get("[name='j_password']").type(password)
+    cy.get(".btn-lg").click()
+})
